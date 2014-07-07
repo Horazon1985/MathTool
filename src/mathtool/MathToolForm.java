615becd4a88e3c0c9436964a1c62feb0c588e7ab
sergei <sergei@jass.mathematik.uni-freiburg.de>
2014-07-07 15:50:16 +0200

    Update.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -184,7 +184,7 @@ public class MathToolForm extends javax.swing.JFrame implements KeyListener{
         getContentPane().add(InputButton);
         InputButton.setBounds(518, 335, 91, 25);
 
-        InputField.setText("plot(x^2+y^2,-1,1,-1,1)");
+        InputField.setText("plot(x^2-y^2,-1,1,-1,1)");
         getContentPane().add(InputField);
         InputField.setBounds(10, 336, 490, 19);
 
