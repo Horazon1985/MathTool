3eb3c36bb3aa678aec21331d1406eb4722dd247a
sergei <sergei@jass.mathematik.uni-freiburg.de>
2014-07-08 15:54:59 +0200

    Update.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -182,11 +182,11 @@ public class MathToolForm extends javax.swing.JFrame implements KeyListener{
             }
         });
         getContentPane().add(InputButton);
-        InputButton.setBounds(518, 335, 71, 23);
+        InputButton.setBounds(518, 335, 91, 25);
 
-        InputField.setText("plot(x^2+y^2,-1,1,-1,1)");
+        InputField.setText("plot(x^2-y^2,0.1,2,0.1,2)");
         getContentPane().add(InputField);
-        InputField.setBounds(10, 336, 490, 20);
+        InputField.setBounds(10, 336, 490, 19);
 
         RotateButton.setText("3D-Graphen rotieren lassen");
         RotateButton.addActionListener(new java.awt.event.ActionListener() {
@@ -195,7 +195,7 @@ public class MathToolForm extends javax.swing.JFrame implements KeyListener{
             }
         });
         getContentPane().add(RotateButton);
-        RotateButton.setBounds(10, 376, 165, 23);
+        RotateButton.setBounds(10, 376, 231, 25);
 
         jMenu1.setText("Datei");
         jMenuBar1.add(jMenu1);
