fd08b32d9cf94531639657f9a3ddf4a1b1db6933
sergei <sergei@jass.mathematik.uni-freiburg.de>
2014-07-29 15:03:04 +0200

    Update.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -220,7 +220,7 @@ public class MathToolForm extends javax.swing.JFrame implements KeyListener{
         getContentPane().add(InputButton);
         InputButton.setBounds(518, 335, 91, 25);
 
-        InputField.setText("plot(x^2,-1,2)");
+        InputField.setText("plot(x*sin(1/x),-2,2)");
         getContentPane().add(InputField);
         InputField.setBounds(10, 336, 490, 19);
 
