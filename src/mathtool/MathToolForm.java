24e15310724d0fb9b32ba69bd3ed8e377992a169
Sergio <Sergio@192.168.2.101>
2014-08-01 18:31:18 +0200

    Update.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -220,7 +220,7 @@ public class MathToolForm extends javax.swing.JFrame implements KeyListener{
         getContentPane().add(InputButton);
         InputButton.setBounds(518, 335, 71, 23);
 
-        InputField.setText("solvedgl(1,x,1,0,10,0)");
+        InputField.setText("solvedgl(-0.5*y'-10*y,x,2,0,10,2,-1)");
         getContentPane().add(InputField);
         InputField.setBounds(10, 336, 490, 20);
 
