8ff11a6fd336991b244d9ce186858405ee1534ee
Sergio <Sergio@192.168.2.101>
2014-08-09 13:27:59 +0200

    Update.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -218,11 +218,9 @@ public class MathToolForm extends javax.swing.JFrame implements KeyListener{
             }
         });
         getContentPane().add(InputButton);
-        InputButton.setBounds(518, 335, 91, 25);
-
-        InputField.setText("(a^64564545642341245)^445744454453464.1");
+        InputButton.setBounds(518, 335, 71, 23);
         getContentPane().add(InputField);
-        InputField.setBounds(10, 336, 490, 19);
+        InputField.setBounds(10, 336, 490, 20);
 
         RotateButton.setText("3D-Graphen rotieren lassen");
         RotateButton.addActionListener(new java.awt.event.ActionListener() {
@@ -231,7 +229,7 @@ public class MathToolForm extends javax.swing.JFrame implements KeyListener{
             }
         });
         getContentPane().add(RotateButton);
-        RotateButton.setBounds(10, 376, 231, 25);
+        RotateButton.setBounds(10, 376, 165, 23);
 
         jMenu1.setText("Datei");
         jMenuBar1.add(jMenu1);
