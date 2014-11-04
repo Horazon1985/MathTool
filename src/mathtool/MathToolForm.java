b2bec8636891451fdfb59099dba7110a8f8c18ee
Sergio <Sergio@192.168.2.101>
2014-11-04 23:30:30 +0100

    Update - Vereinfachung von abs und sgn (in Arbeit).

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -295,6 +295,7 @@ public class MathToolForm extends javax.swing.JFrame implements MouseListener {
         inputButton.setBounds(518, 335, 100, 30);
 
         inputField.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        inputField.setText("sgn(((-2)*x)/(5*y))");
         inputField.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 inputFieldKeyPressed(evt);
