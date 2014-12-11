9b832d2a26756d82752d14d2c35b5f78ca0802a1
Sergio <Sergio@192.168.2.101>
2014-12-11 23:14:45 +0100

    Update.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -299,7 +299,7 @@ public class MathToolForm extends javax.swing.JFrame implements MouseListener {
         inputButton.setBounds(518, 335, 100, 30);
 
         inputField.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
-        inputField.setText("solve(8*sin(x)^3-sin(x)-1/2=0)");
+        inputField.setText("solve(14+sin(2*x-7)-4*x=0)");
         inputField.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 inputFieldKeyPressed(evt);
