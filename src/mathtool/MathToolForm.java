68521bccd2443c7a30c1f769e4165e85e9585334
Sergio <Sergio@192.168.2.101>
2015-01-04 00:16:56 +0100

    Update - In Arbeit: Bugs beseitigen.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -299,6 +299,7 @@ public class MathToolForm extends javax.swing.JFrame implements MouseListener {
         inputButton.setBounds(518, 335, 100, 30);
 
         inputField.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        inputField.setText("expand((1/cos(x))*(sin(x)/cos(x)))");
         inputField.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 inputFieldKeyPressed(evt);
