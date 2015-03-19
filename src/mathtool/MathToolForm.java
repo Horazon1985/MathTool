7dfdcdce2aae50906eada7b4a13a58f3f508facc
Sergio <Sergio@Sergio-PC>
2015-03-19 17:14:40 +0100

    Update.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -470,7 +470,6 @@ public class MathToolForm extends JFrame implements MouseListener {
         inputButton.setBounds(518, 335, 100, 30);
 
         inputField.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
-        inputField.setText("(15*((13*((11*((9*((7*((5*((3*(x/2-a))/4-a))/6-a))/8-a))/10-a))/12-a))/14-a))/16");
         inputField.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 inputFieldKeyPressed(evt);
