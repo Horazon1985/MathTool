192228c16dcba9c2a3b4b529c22532a56c029bf7
Sergio <Sergio@192.168.2.101>
2015-04-16 17:32:45 +0200

    Update - Refactoring (in Arbeit); ein paar Bugs ausgeräumt.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -471,6 +471,7 @@ public class MathToolForm extends JFrame implements MouseListener {
         inputButton.setBounds(518, 335, 100, 30);
 
         inputField.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
+        inputField.setText("solve(exp(6*x)-5*exp(3*x)+1=0)");
         inputField.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 inputFieldKeyPressed(evt);
