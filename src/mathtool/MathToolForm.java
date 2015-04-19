7c5bfd515d674c40c6b28703d44f17dd426301cf
Sergei <Sergei@192.168.2.101>
2015-04-19 23:16:09 +0200

    Update - Refactoring (in Arbeit); einige Bugs gefixt.

--- src/mathtool/MathToolForm.java
+++ src/mathtool/MathToolForm.java
@@ -471,7 +471,7 @@ public class MathToolForm extends JFrame implements MouseListener {
         inputButton.setBounds(518, 335, 100, 30);
 
         inputField.setFont(new java.awt.Font("Verdana", 0, 12)); // NOI18N
-        inputField.setText("def(f(x)=sum(k*x^k,k,1,100))");
+        inputField.setText("taylordeq(-y,t,2,0,1,0,10)");
         inputField.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 inputFieldKeyPressed(evt);
