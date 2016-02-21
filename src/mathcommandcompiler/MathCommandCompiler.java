ae0d0369c363cb1a3ff26573970c752d109967c7
Sergei Kovalenko <kovalenko@arcor.de>
2016-02-21 10:25:10 +0100

    Update - Kleinen Bug in GraphicPanelCylindrical behoben -> MathCommandCompiler angepasst.

--- src/mathcommandcompiler/MathCommandCompiler.java
+++ src/mathcommandcompiler/MathCommandCompiler.java
@@ -2492,6 +2492,39 @@ public abstract class MathCommandCompiler {
         Expression phi_0 = ((Expression) command.getParams()[command.getParams().length - 2]).simplify(simplifyTypesPlot);
         Expression phi_1 = ((Expression) command.getParams()[command.getParams().length - 1]).simplify(simplifyTypesPlot);
 
+        // Validierung der Zeichenbereichsgrenzen.
+        double minR = r_0.evaluate();
+        double maxR = r_1.evaluate();
+        double minPhi = phi_0.evaluate();
+        double maxPhi = phi_1.evaluate();
+
+        if (minR < 0) {
+            throw new EvaluationException(Translator.translateExceptionMessage("MCC_MIN_RADIUS_MUST_BE_NONNEGATIVE_IN_PLOTCYLINDRICAL_1")
+                    + (exprs.size() + 3)
+                    + Translator.translateExceptionMessage("MCC_MIN_RADIUS_MUST_BE_NONNEGATIVE_IN_PLOTCYLINDRICAL_2"));
+        }
+        if (minR >= maxR) {
+            throw new EvaluationException(Translator.translateExceptionMessage("MCC_LIMITS_MUST_BE_WELL_ORDERED_IN_PLOTCYLINDRICAL_1")
+            + (exprs.size() + 3)
+            + Translator.translateExceptionMessage("MCC_LIMITS_MUST_BE_WELL_ORDERED_IN_PLOTCYLINDRICAL_2")
+            + (exprs.size() + 4)
+            + Translator.translateExceptionMessage("MCC_LIMITS_MUST_BE_WELL_ORDERED_IN_PLOTCYLINDRICAL_3"));
+        }
+        if (minPhi >= maxPhi) {
+            throw new EvaluationException(Translator.translateExceptionMessage("MCC_LIMITS_MUST_BE_WELL_ORDERED_IN_PLOTCYLINDRICAL_1")
+            + (exprs.size() + 5)
+            + Translator.translateExceptionMessage("MCC_LIMITS_MUST_BE_WELL_ORDERED_IN_PLOTCYLINDRICAL_2")
+            + (exprs.size() + 6)
+            + Translator.translateExceptionMessage("MCC_LIMITS_MUST_BE_WELL_ORDERED_IN_PLOTCYLINDRICAL_3"));
+        }
+        if (maxPhi - minPhi > 20 * Math.PI) {
+            throw new EvaluationException(Translator.translateExceptionMessage("MCC_DIFFERENCE_OF_ANGLES_MUST_BE_AT_MOST_20_PI_IN_PLOTCYLINDRICAL_1")
+                    + (exprs.size() + 5)
+                    + Translator.translateExceptionMessage("MCC_DIFFERENCE_OF_ANGLES_MUST_BE_AT_MOST_20_PI_IN_PLOTCYLINDRICAL_2")
+                    + (exprs.size() + 6)
+                    + Translator.translateExceptionMessage("MCC_DIFFERENCE_OF_ANGLES_MUST_BE_AT_MOST_20_PI_IN_PLOTCYLINDRICAL_3"));
+        }
+        
         // Graphen zeichnen.
         graphicPanelCylindrical3D.setParameters((String) command.getParams()[command.getParams().length - 6], (String) command.getParams()[command.getParams().length - 5], 150, 200, 30, 30);
         graphicPanelCylindrical3D.drawCylindricalGraphs3D(r_0, r_1, phi_0, phi_1, exprs);
