5581f97669ac3e4678eb390172b9c8646ef2bf19
kovalens <kovalens@MSGN01630.int.root.msg.ag>
2017-04-13 14:44:49 +0200

    Noch weiter an der Struktur des AlgorithmExecutor gearbeitet (Befehlsstrukturen).

--- src/algorithmexecutor/command/AssignValueCommand.java
+++ src/algorithmexecutor/command/AssignValueCommand.java
@@ -1,5 +1,29 @@
 package algorithmexecutor.command;
 
+import algorithmexecutor.exceptions.AlgorithmCompileException;
+import algorithmexecutor.exceptions.CompileExceptionTexts;
+import algorithmexecutor.identifier.Identifier;
+
 public class AssignValueCommand extends AlgorithmCommand {
+    
+    private final Identifier identifierSrc;
+    private final Identifier identifierTarget;
+    
+    public AssignValueCommand(Identifier identifierSrc, Identifier identifierTarget) throws AlgorithmCompileException {
+        if (identifierSrc.getType() != identifierTarget.getType()) {
+            throw new AlgorithmCompileException(CompileExceptionTexts.INCOMPATIBEL_TYPES);
+        }
+        this.identifierSrc = identifierSrc;
+        this.identifierTarget = identifierTarget;
+    }
+
+    public Identifier getIdentifierTarget() {
+        return identifierTarget;
+    }
+
+    public Identifier getIdentifierSrc() {
+        return identifierSrc;
+    }
+    
     
 }
--- src/algorithmexecutor/exceptions/CompileExceptionTexts.java
+++ src/algorithmexecutor/exceptions/CompileExceptionTexts.java
@@ -3,7 +3,6 @@ package algorithmexecutor.exceptions;
 public abstract class CompileExceptionTexts {
     
     public static final String IDENTIFIER_ALREADY_DEFINED = "Identifier {0} is already defined.";
-    
-    
+    public static final String INCOMPATIBEL_TYPES = "Incompatible types: {0} cannot be converted to {1}..";    
     
 }
