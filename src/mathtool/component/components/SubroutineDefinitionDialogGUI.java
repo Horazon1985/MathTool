8f2cae9c4f34680c315e421d38d41622a6d82b98
Sergei <Sergei@LAPTOP-9MC4FDMD.Speedport_W_724V_01011603_00_008>
2017-11-16 00:37:26 +0100

    GUI für das Generieren von Subroutinen fast fertig.

--- src/mathtool/component/components/SubroutineDefinitionDialogGUI.java
+++ src/mathtool/component/components/SubroutineDefinitionDialogGUI.java
@@ -26,19 +26,24 @@ public class SubroutineDefinitionDialogGUI extends JDialog {
     private static final String ICON_PATH = "/mathtool/icons/MathToolIcon.png";
 
     private static final String TITLE_ID = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE";
-    private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER";
-    private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_TYPE = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_TYPE";
     private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_NAME = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_NAME";
+    private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_RETURN_TYPE = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_RETURN_TYPE";
+    private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER";
+    private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER_TYPE = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER_TYPE";
+    private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER_NAME = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER_NAME";
     private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_GENERAL_PARAMETER = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_GENERAL_PARAMETER";
     private static final String GUI_MathToolAlgorithmsGUI_CODE_ADD_PARAMETER_BUTTON = "GUI_MathToolAlgorithmsGUI_CODE_ADD_PARAMETER_BUTTON";
-    private static final String GUI_MathToolAlgorithmsGUI_CODE_REMOVE_PARAMETER_BUTTON = "GUI_MathToolAlgorithmsGUI_CODE_REMOVE_PARAMETER_BUTTON";
     private static final String GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_BUTTON = "GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_BUTTON";
 
     private static JTextArea algorithmEditor;
 
     private JLabel routineNameLabel;
 
+    private JTextField routineNameField;
+    
     private JLabel routineReturnTypeLabel;
+
+    private JComboBox<IdentifierType> routineReturnTypeCombobox;
     
     private JLabel parameterLabel;
 
@@ -59,7 +64,7 @@ public class SubroutineDefinitionDialogGUI extends JDialog {
     private JButton generateButton;
 
     int dialogWidth = 500;
-    int dialogHeight = 200;
+    int dialogHeight = 275;
 
     private static SubroutineDefinitionDialogGUI instance = null;
 
@@ -79,29 +84,44 @@ public class SubroutineDefinitionDialogGUI extends JDialog {
     private void init(int algorithmGuiX, int algorithmGuiY, int algorithmGuiWidth, int algorithmGuiHeigh) {
         initBounds(algorithmGuiX, algorithmGuiY, algorithmGuiWidth, algorithmGuiHeigh);
 
+        this.routineNameLabel = new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_NAME));
+        this.add(this.routineNameLabel);
+        this.routineNameLabel.setBounds(25, 25, 100, 25);
+        
+        this.routineNameField = new JTextField();
+        this.add(this.routineNameField);
+        this.routineNameField.setBounds(150, 25, 200, 25);
+
+        this.routineReturnTypeLabel = new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_RETURN_TYPE));
+        this.add(this.routineReturnTypeLabel);
+        this.routineReturnTypeLabel.setBounds(25, 75, 100, 25);
+
+        this.routineReturnTypeCombobox = new JComboBox<>(IdentifierType.values());
+        this.add(this.routineReturnTypeCombobox);
+        this.routineReturnTypeCombobox.setBounds(150, 75, 200, 25);
+        
         this.parameterLabel = new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER));
         this.add(this.parameterLabel);
-        this.parameterLabel.setBounds(25, 25, 75, 25);
+        this.parameterLabel.setBounds(25, 125, 75, 25);
 
-        this.typeLabel = new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_TYPE));
+        this.typeLabel = new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER_TYPE));
         this.add(this.typeLabel);
-        this.typeLabel.setBounds(125, 25, 75, 25);
+        this.typeLabel.setBounds(125, 125, 75, 25);
 
-        this.nameLabel = new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_NAME));
+        this.nameLabel = new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_PARAMETER_NAME));
         this.add(this.nameLabel);
-        this.nameLabel.setBounds(225, 25, 75, 25);
+        this.nameLabel.setBounds(250, 125, 75, 25);
 
         this.addParameterButton = new JButton(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_ADD_PARAMETER_BUTTON));
         this.add(this.addParameterButton);
-        this.addParameterButton.setBounds(25, 75, 200, 25);
+        this.addParameterButton.setBounds(25, 175, 200, 25);
         this.addParameterButton.addActionListener((ActionEvent e) -> {
             addNewParameter();
-            
         });
 
         this.generateButton = new JButton(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_BUTTON));
         this.add(this.generateButton);
-        this.generateButton.setBounds(250, 75, 200, 25);
+        this.generateButton.setBounds(250, 175, 200, 25);
         this.generateButton.addActionListener((ActionEvent e) -> {
             
         });
@@ -120,8 +140,23 @@ public class SubroutineDefinitionDialogGUI extends JDialog {
         this.parameterLabels.add(
                 new JLabel(Translator.translateOutputMessage(GUI_MathToolAlgorithmsGUI_CODE_GENERATE_SUBROUTINE_GENERAL_PARAMETER, this.parameterLabels.size() + 1)));
         this.add(this.parameterLabels.get(this.parameterLabels.size() - 1));
-        this.parameterLabels.get(this.parameterLabels.size() - 1).setBounds(25, 25 + 50 * this.parameterLabels.size(), 50, 25);
+        this.parameterLabels.get(this.parameterLabels.size() - 1).setBounds(25, 125 + 50 * this.parameterLabels.size(), 100, 25);
 
+        this.comboBoxes.add(new JComboBox(IdentifierType.values()));
+        this.add(this.comboBoxes.get(this.comboBoxes.size() - 1));
+        this.comboBoxes.get(this.comboBoxes.size() - 1).setBounds(125, 125 + 50 * this.parameterLabels.size(), 100, 25);
+        
+        this.textFields.add(new JTextField());
+        this.add(this.textFields.get(this.textFields.size() - 1));
+        this.textFields.get(this.textFields.size() - 1).setBounds(250, 125 + 50 * this.parameterLabels.size(), 150, 25);
+
+        this.removeButtons.add(new JButton("X"));
+        this.add(this.removeButtons.get(this.removeButtons.size() - 1));
+        this.removeButtons.get(this.removeButtons.size() - 1).setBounds(415, 125 + 50 * this.parameterLabels.size(), 35, 25);
+        this.removeButtons.get(this.removeButtons.size() - 1).addActionListener((ActionEvent e) -> {
+            
+        });
+        
         relocateButtonsAfterNewParameter();
         updateBounds();
         revalidate();
@@ -135,14 +170,14 @@ public class SubroutineDefinitionDialogGUI extends JDialog {
     }
 
     private void updateBounds() {
-        this.dialogHeight = 100 + 50 * this.parameterLabels.size();
+        this.dialogHeight = 275 + 50 * this.parameterLabels.size();
         setBounds(getX(), getY(), getWidth(), this.dialogHeight);
     }
 
     private void relocateButtonsAfterNewParameter() {
-        this.addParameterButton.setBounds(this.addParameterButton.getX(), 75 + 50 * this.parameterLabels.size(), 
+        this.addParameterButton.setBounds(this.addParameterButton.getX(), 175 + 50 * this.parameterLabels.size(), 
                 this.addParameterButton.getWidth(), this.addParameterButton.getHeight());
-        this.generateButton.setBounds(this.generateButton.getX(), 75 + 50 * this.parameterLabels.size(), 
+        this.generateButton.setBounds(this.generateButton.getX(), 175 + 50 * this.parameterLabels.size(), 
                 this.generateButton.getWidth(), this.generateButton.getHeight());
     }
 
